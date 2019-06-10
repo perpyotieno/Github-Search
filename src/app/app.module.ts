cb70ad7034c06249afe5394506abee21ddac1958
Perpetua Mary <perpetuamary@yahoo.com>
2019-06-10 04:46:23 +0300

    import routing module to the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,39 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-// import {FormsModule} from '@angular/forms';
-// import { HttpClientModule} from '@angular/common/http';
-// import { RouterModule, Routes } from '@angular/router';
-// import {RoutingModule} from './routing/routing.module'
-//
-// import { GitsearcherService } from 'src/app/Gitsearch/gitsearcher.service'
-//
-//
-// import { AppComponent } from './app.component';
-// import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
-// import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
-// import { RepositoriesComponent } from 'src/app/repositories/repositories.component';
-//
-//
-// const routes:Routes=[
-//   {path:"gitsearch",component:GitsearchComponent},
-//   {path:"repositories",component:RepositoriesComponent}
-// ]
-//
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     GitsearchFormComponent,
-//     GitsearchComponent,
-//     RepositoriesComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     FormsModule,
-//     HttpClientModule,
-//     RouterModule.forRoot(routes),
-//     RoutingModule,
-//   ],
-//   providers: [GitsearcherService],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import {RoutingModule} from './routing/routing.module'
+
+import { GitsearcherService } from 'src/app/Gitsearch/gitsearcher.service'
+
+
+import { AppComponent } from './app.component';
+import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
+import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
+import { RepositoriesComponent } from 'src/app/repositories/repositories.component';
+
+
+const routes:Routes=[
+  {path:"gitsearch",component:GitsearchComponent},
+  {path:"repositories",component:RepositoriesComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GitsearchFormComponent,
+    GitsearchComponent,
+    RepositoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    RoutingModule,
+  ],
+  providers: [GitsearcherService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
