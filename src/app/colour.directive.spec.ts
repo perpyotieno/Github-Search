173d3b8d2d4dcad43172b94cdc08b2dfb765e27b
Perpetua Mary <perpetuamary@yahoo.com>
2019-06-10 12:10:01 +0300

    Auto-generated commit

--- src/app/colour.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ColourDirective } from './colour.directive';
-
-describe('ColourDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ColourDirective();
-    expect(directive).toBeTruthy();
-  });
-});
