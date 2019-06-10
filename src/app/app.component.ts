d129d13060465c3c8bedd320619e426cdb22d8fe
Perpetua Mary <perpetuamary@yahoo.com>
2019-06-10 04:21:12 +0300

    Make the first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+// import { Component } from '@angular/core';
+// import {GitsearcherService} from 'src/app/Gitsearch/gitsearcher.service'
+// import { GitsearchFormComponent } from 'src/app/gitsearch-form/gitsearch-form.component'
+// import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
+//
+//
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Gitsearch';
+// }
