b97e81afc03ca97c97766ca8a83d780295f5937c
Perpetua Mary <perpetuamary@yahoo.com>
2019-06-10 04:26:27 +0300

    declare the username and client details variables

--- src/app/Gitsearch/gitsearcher.service.ts
+++ src/app/Gitsearch/gitsearcher.service.ts
@@ -7,16 +7,16 @@ import { GitsearchComponent } from 'src/app/gitsearch/gitsearch.component';
 import {FormsModule} from '@angular/forms';
 
 
-// @Injectable()
-// export class GitsearcherService {
-//
-//   private username:string;
-//   private clientdetails: string ='361808b989d3d49cd11638e1f5659d553dd49c5b';
-//
-//   constructor( private http:HttpClient) {
-//   console.log("service is now ready");
-//
-// }
+@Injectable()
+export class GitsearcherService {
+
+  private username:string;
+  private clientdetails: string ='361808b989d3d49cd11638e1f5659d553dd49c5b';
+
+  constructor( private http:HttpClient) {
+  console.log("service is now ready");
+
+}
 // getProfileInfo(username){
 //   console.log(this.username)
 //   return this.http.get("https://api.github.com/users/" + username + "?access_token=" + this.clientdetails)
